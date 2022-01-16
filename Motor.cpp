ae44994ec1a57eba30c1c3c7637b4b80a75b9917
yutotnh <57719497+yutotnh@users.noreply.github.com>
2022-01-16 14:27:02 +0000

    Committing clang-format changes

--- Motor.cpp
+++ Motor.cpp
@@ -3,71 +3,47 @@
 namespace spirit {
 
 Motor::Motor()
-    : _duty_cycle(0.0F), _state(default_state), _pulse_period(default_pulse_period)
-{
+    : _duty_cycle(0.0F), _state(default_state),
+      _pulse_period(default_pulse_period) {}
+
+void Motor::duty_cycle(float value) {
+  if ((0.00F <= value) && (value <= 1.00F)) {
+    _duty_cycle = value;
+  }
 }
 
-void Motor::duty_cycle(float value)
-{
-    if ((0.00F <= value) && (value <= 1.00F)) {
-        _duty_cycle = value;
-    }
+Motor &Motor::operator=(const float value) {
+  duty_cycle(value);
+  return *this;
 }
 
-Motor &Motor::operator=(const float value)
-{
-    duty_cycle(value);
-    return *this;
+float Motor::duty_cycle() const { return _duty_cycle; }
+
+Motor::operator float() { return duty_cycle(); }
+
+void Motor::state(State type) {
+  if ((State::Begin <= type) && (type < State::End)) {
+    _state = type;
+  }
 }
 
-float Motor::duty_cycle() const
-{
-    return _duty_cycle;
+Motor &Motor::operator=(const State type) {
+  state(type);
+  return *this;
 }
 
-Motor::operator float()
-{
-    return duty_cycle();
+State Motor::state() const { return _state; }
+
+Motor::operator State() { return state(); }
+
+void Motor::pulse_period(float seconds) {
+  if ((min_pulse_period <= seconds) && (seconds <= max_pulse_period)) {
+    _pulse_period = seconds;
+  }
 }
 
-void Motor::state(State type)
-{
-    if ((State::Begin <= type) && (type < State::End)) {
-        _state = type;
-    }
-}
+float Motor::pulse_period() const { return _pulse_period; }
 
-Motor &Motor::operator=(const State type)
-{
-    state(type);
-    return *this;
-}
+void Motor::frequency(float hz) { pulse_period(1.0F / hz); }
 
-State Motor::state() const
-{
-    return _state;
-}
-
-Motor::operator State()
-{
-    return state();
-}
-
-void Motor::pulse_period(float seconds)
-{
-    if ((min_pulse_period <= seconds) && (seconds <= max_pulse_period)) {
-        _pulse_period = seconds;
-    }
-}
-
-float Motor::pulse_period() const
-{
-    return _pulse_period;
-}
-
-void Motor::frequency(float hz)
-{
-    pulse_period(1.0F / hz);
-}
-
-}  // namespace spirit
+} // namespace spirit
